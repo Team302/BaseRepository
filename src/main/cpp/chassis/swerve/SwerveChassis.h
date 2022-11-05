8864f41e521cd1334b6d69dbfd05c511fd819df9
Nick Frisch <nfrisch3@outlook.com>
2022-11-05 12:11:35 -0400

    Added essential stuff from past SwerveChassis

--- src/main/cpp/chassis/swerve/SwerveChassis.h
+++ src/main/cpp/chassis/swerve/SwerveChassis.h
@@ -23,13 +23,40 @@
 
 //Team302 Includes
 #include <states/chassis/SwerveDriveState.h>
+
 #include <chassis/swerve/ISwerveDriveOrientation.h>
 #include <chassis/swerve/SwerveEnums.h>
+#include <chassis/swerve/SwerveModule.h>
+#include <chassis/swerve/SwerveOdometry.h>
 
 class SwerveChassis
 {
     public:
-        SwerveChassis();
+        /// @brief Construct a SwerveChassis
+        /// @param [in] std::shared_ptr<SwerveModule>           frontleft:          front left swerve module
+        /// @param [in] std::shared_ptr<SwerveModule>           frontright:         front right swerve module
+        /// @param [in] std::shared_ptr<SwerveModule>           backleft:           back left swerve module
+        /// @param [in] std::shared_ptr<SwerveModule>           backright:          back right swerve module
+        /// @param [in] units::length::inch_t                   wheelDiameter:      Diameter of the wheel
+        /// @param [in] units::length::inch_t                   wheelBase:          distance between the front and rear wheels
+        /// @param [in] units::length::inch_t                   track:              distance between the left and right wheels
+        /// @param [in] units::velocity::meters_per_second_t    maxSpeed:           maximum linear speed of the chassis 
+        /// @param [in] units::radians_per_second_t             maxAngularSpeed:    maximum rotation speed of the chassis 
+        /// @param [in] double                                  maxAcceleration:    maximum acceleration in meters_per_second_squared
+        SwerveChassis
+        (
+            std::shared_ptr<SwerveModule>                               frontLeft, 
+			std::shared_ptr<SwerveModule>                               frontRight,
+			std::shared_ptr<SwerveModule>                               backLeft, 
+			std::shared_ptr<SwerveModule>                               backRight, 
+            units::length::inch_t                                       wheelDiameter,
+			units::length::inch_t                                       wheelBase,
+			units::length::inch_t                                       track,
+			units::velocity::meters_per_second_t                        maxSpeed,
+			units::radians_per_second_t                                 maxAngularSpeed,
+			units::acceleration::meters_per_second_squared_t            maxAcceleration,
+			units::angular_acceleration::radians_per_second_squared_t   maxAngularAcceleration
+        );
 
         /// @brief Runs the current SwerveDriveState
         void Drive();
@@ -37,6 +64,20 @@ class SwerveChassis
         /// @brief Initializes the targetState, updates the current SwerveDriveState
         /// @param [in] SwerveDriveState    targetState - the new state that should be ran
         void Drive(SwerveDriveState* targetState);
+
+        /// Getters
+
+        units::length::inch_t GetWheelDiameter() const {return m_wheelDiameter; }  
+        units::length::inch_t GetWheelBase() const {return m_wheelBase; }  
+        units::length::inch_t GetTrack() const {return m_track;}
+        units::velocity::meters_per_second_t GetMaxSpeed() const {return m_maxSpeed;}
+        units::radians_per_second_t GetMaxAngularSpeed() const {return m_maxAngularSpeed;}
+        units::acceleration::meters_per_second_squared_t GetMaxAcceleration() const { return m_maxAcceleration; }
+        units::angular_acceleration::radians_per_second_squared_t GetMaxAngularAcceleration() const { return m_maxAngularAcceleration; }
+        std::shared_ptr<SwerveModule> GetFrontLeft() const { return m_frontLeft;}
+        std::shared_ptr<SwerveModule> GetFrontRight() const { return m_frontRight;}
+        std::shared_ptr<SwerveModule> GetBackLeft() const { return m_backLeft;}
+        std::shared_ptr<SwerveModule> GetBackRight() const { return m_backRight;};
 
         /// @brief Returns the current SwerveDriveState
         /// @return SwerveDriveState* - current SwerveDriveState
@@ -62,6 +103,19 @@ class SwerveChassis
         ISwerveDriveOrientation* GetOrientation();
     
     private:
+        std::shared_ptr<SwerveModule>                                       m_frontLeft;
+        std::shared_ptr<SwerveModule>                                       m_frontRight;
+        std::shared_ptr<SwerveModule>                                       m_backLeft;
+        std::shared_ptr<SwerveModule>                                       m_backRight;
+        
+        units::length::inch_t                                               m_wheelDiameter;       
+        units::length::inch_t                                               m_wheelBase;       
+        units::length::inch_t                                               m_track;
+        units::velocity::meters_per_second_t                                m_maxSpeed;
+        units::radians_per_second_t                                         m_maxAngularSpeed;
+        units::acceleration::meters_per_second_squared_t                    m_maxAcceleration;
+        units::angular_acceleration::radians_per_second_squared_t           m_maxAngularAcceleration;
+
         //Dont know if this and orientation should be vectors or maps, b/c of GetDriveState: I feel like they should be maps
         //std::vector<SwerveDriveState*>          m_swerveDriveStates;
         SwerveDriveState*                                                   m_currentDriveState;
@@ -71,4 +125,6 @@ class SwerveChassis
         //std::vector<ISwerveDriveOrientation*>   m_swerveOrientation;
         std::map<SwerveEnums::HeadingOption, ISwerveDriveOrientation*>      m_swerveOrientation;
         ISwerveDriveOrientation*                                            m_currentOrientation;
+
+        SwerveOdometry*                                                     m_odometry;
 };
